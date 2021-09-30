74da8943944041065af4ffe8083ba65edcf5189d
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2021-09-30 18:46:42 +0200

    Create ui module and move activity to it

--- ui/src/main/java/com/mongodb/flashcards/ui/MainActivity.kt
+++ ui/src/main/java/com/mongodb/flashcards/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.mongodb.flashcards.app
+package com.mongodb.flashcards.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -8,7 +8,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.mongodb.flashcards.app.ui.theme.FlashCardsTheme
+import com.mongodb.flashcards.ui.theme.FlashCardsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
